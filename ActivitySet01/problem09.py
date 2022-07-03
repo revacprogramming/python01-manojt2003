bd26ce3ee29f2a0ab7626bea6e1185d10b8cfd28
Manoj T <96277980+manojt2003@users.noreply.github.com>
2022-07-03 05:30:45 +0000

    problem09

--- ActivitySet01/problem09.py
+++ ActivitySet01/problem09.py
@@ -1,3 +1,13 @@
-# Lists
-
-filename = "dataset/romeo.txt"
+fname = input("Enter file name: ")
+fh = open(fname)
+lst = list()
+for line in fh:
+    word=line.rstrip()
+    word=line.split()
+    for i in word:
+        if i in lst:
+            continue
+        else:
+            lst.append(i)
+lst.sort()
+print (lst)
